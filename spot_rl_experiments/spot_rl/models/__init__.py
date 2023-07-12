b718f57e2e594f4107f1437586f898f702ab68b2
Jimmy Yang <jimmytyyang@meta.com>
2023-07-12 16:10:39 -0400

    Add LICENSE.md file, and watermark to all .py files

--- spot_rl_experiments/spot_rl/models/__init__.py
+++ spot_rl_experiments/spot_rl/models/__init__.py
@@ -1,2 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 from spot_rl.models.owlvit import OwlVit
 from spot_rl.models.sentence_similarity import SentenceSimilarity
