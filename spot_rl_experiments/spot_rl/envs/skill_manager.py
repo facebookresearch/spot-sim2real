140bb7ddae4524dee7aa560faf107bed0153a8ab
Tushar Sangam <tusharsangam5@gmail.com>
2023-12-05 15:15:04 -0800

    skill manager update for apt testing

--- spot_rl_experiments/spot_rl/envs/skill_manager.py
+++ spot_rl_experiments/spot_rl/envs/skill_manager.py
@@ -372,14 +372,51 @@ class SpotSkillManager:
 if __name__ == "__main__":
     from spot_rl.utils.utils import map_user_input_to_boolean
 
-    spotskillmanager = SpotSkillManager(use_mobile_pick=True)
-
+    pick_targets = {
+        "kitchen": (
+            3.8482142244527835,
+            -3.4519528625906206,
+            np.deg2rad(-89.14307672622927),
+        ),
+        "kitchen_-10": (
+            3.8482142244527835,
+            -3.4519528625906206,
+            np.deg2rad(-89.14307672622927 - 10.0),
+        ),
+        "kitchen_+10": (
+            3.8482142244527835,
+            -3.4519528625906206,
+            np.deg2rad(-89.14307672622927 + 10.0),
+        ),
+        "table": (4.979398852803741, 3.535594946585519, -0.008869974097951427),
+        "table_+10": (
+            4.979398852803741,
+            3.535594946585519,
+            np.deg2rad(np.rad2deg(-0.008869974097951427) + 15.0),
+        ),
+        "table_-10": (
+            4.979398852803741,
+            3.535594946585519,
+            np.deg2rad(np.rad2deg(-0.008869974097951427) - 15.0),
+        ),
+    }
+    # pick_from = "kitchen"
+    # place_to = "sofa"
     # Testing Mobile Gaze
     contnue = True
-    while contnue:
-        spotskillmanager.nav("pos4")
-        x = input("Press Enter to continue to mobile gaze ")
-        spotskillmanager.pick("toy_lion")
+    i: int = 2
+    n: int = len(pick_targets)
+    pick_targets_keys = list(pick_targets.keys())
+    while contnue and i < 3:
+        pick_from = pick_targets_keys[i]
+        i += 1
+        spotskillmanager = SpotSkillManager(use_mobile_pick=True)
+        spotskillmanager.nav(*pick_targets[pick_from])
+        x = input(f"Press Enter to continue to mobile gaze from {pick_from}")
+        # spotskillmanager.pick("toy_lion")
+        spotskillmanager.pick(
+            "cereal_box"
+        )  # if i < 3 else spotskillmanager.pick("toy_lion")
         spotskillmanager.spot.open_gripper()
         contnue = map_user_input_to_boolean("Do you want to do it again ? Y/N ")
 
