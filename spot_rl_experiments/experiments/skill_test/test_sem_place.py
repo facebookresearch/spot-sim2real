cc661dd6971fa4993087ef3ea588efceb83cef1c
Tushar Sangam <tusharsangam5@gmail.com>
2024-02-13 18:01:03 -0800

    torchfix

--- spot_rl_experiments/experiments/skill_test/test_sem_place.py
+++ spot_rl_experiments/experiments/skill_test/test_sem_place.py
@@ -9,27 +9,13 @@ from spot_rl.envs.skill_manager import SpotSkillManager
 if __name__ == "__main__":
     from spot_rl.utils.utils import map_user_input_to_boolean
 
-    # Pick Targets from Aria with +- 10 degree angle change
-    pick_targets = [
-        "kitchen",
-        "kitchen_-10",
-        "kitchen_+10",
-        "console",
-        "console_+15",
-        "console_-15",
-    ]
+    place_target = "test_table_lab"
+    spotskillmanager = SpotSkillManager(use_mobile_pick=True, use_semantic_place=True)
     contnue = True
-    i: int = 0
-    n: int = len(pick_targets)
-    while contnue and i < 3:
-        pick_from = pick_targets[i]
-        i += 1
-        spotskillmanager = SpotSkillManager(use_mobile_pick=True)
-        spotskillmanager.nav(pick_from)
-        x = input(f"Press Enter to continue to mobile gaze from {pick_from}")
-        spotskillmanager.pick("creal_box")
-        spotskillmanager.spot.open_gripper()
+    while contnue:
+        spotskillmanager.nav(place_target)
+        spotskillmanager.place(place_target)
         contnue = map_user_input_to_boolean("Do you want to do it again ? Y/N ")
 
     # Navigate to dock and shutdown
-    spotskillmanager.dock()
+    # spotskillmanager.dock()
