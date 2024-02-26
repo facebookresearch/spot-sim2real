b6dbbef6ba839b66e52fe3a7291c971b91fc19eb
Jimmy Yang <yangtsungyen@gmail.com>
2024-02-26 09:24:05 -0800

    v3_24_ckpt_239 is robust to collision

--- spot_rl_experiments/spot_rl/envs/place_env.py
+++ spot_rl_experiments/spot_rl/envs/place_env.py
@@ -53,7 +53,7 @@ class SpotPlaceEnv(SpotBaseEnv):
         #     self.config.SUCC_Z_DIST,
         #     convention="habitat",
         # )
-        place = np.linalg.norm(self.get_place_sensor(True)) < 0.2
+        place = np.linalg.norm(self.get_place_sensor(True)) < 0.25
         print("dis to goal:", np.linalg.norm(self.get_place_sensor(True)))
         print("place in base place env:", place)
         return super().step(
