74ef6e6bd7cfedf485c253ccca0d9dac7b9c7418
Tushar Sangam <tusharsangam5@gmail.com>
2024-03-28 15:18:59 -0700

    Dockerfile add

--- intel_realsense_payload_for_spotsim2real/startpayload.sh
+++ intel_realsense_payload_for_spotsim2real/startpayload.sh
@@ -9,7 +9,7 @@ export PAYLOAD_NAME=IntelRealSenseAuxillaryImageServicePayload
 
 cd /home/spot/Desktop/intel_realsense_payload_for_spotsim2real
 echo "Starting the execution"
-python intelrealsense_image_service.py --guid $PAYLOAD_GUID \
+python3 intelrealsense_image_service.py --guid $PAYLOAD_GUID \
 --secret $PAYLOAD_SECRET --host-ip $PAYLOAD_IP \
 --port $PAYLOAD_PORT $SPOT_IP --jpeg-quality 75 --png-quality 5 --res-width 640 --res-height 480 --filter-depth #\
 #--socket-verification #--show-debug-info
