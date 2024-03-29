f6c3368b4fe31b637e9d7d39ecf93d44ca725a93
Tushar Sangam <tusharsangam5@gmail.com>
2024-03-28 18:16:49 -0700

    cleaning up

--- spot_rl_experiments/spot_rl/utils/search_table_location.py
+++ spot_rl_experiments/spot_rl/utils/search_table_location.py
@@ -9,121 +9,17 @@ import magnum as mn
 import numpy as np
 import open3d as o3d
 import zmq
-from spot_rl.utils.heuristic_nav import (
-    ImageSearch,
-    get_3d_point,
-    get_arguments_for_image_search,
-    pull_back_point_along_theta_by_offset,
-)
+from spot_rl.envs.base_env import SpotBaseEnv
+from spot_rl.utils.heuristic_nav import get_3d_point
 from spot_rl.utils.plane_detection import plane_detect
-from spot_rl.utils.pose_correction import (
-    detect,
-    generate_point_cloud,
-    load_model,
-    segment,
-)
+
+# from spot_rl.utils.pose_correction import (
+#     detect,
+#     generate_point_cloud,
+#     load_model,
+#     segment,
+# )
 from spot_wrapper.spot import Spot, image_response_to_cv2
-
-
-def heurisitic_object_search(
-    x: float,
-    y: float,
-    theta: float,
-    object_target: str,
-    image_search: ImageSearch = None,
-    save_cone_search_images: bool = True,
-    pull_back: bool = True,
-    skillmanager=None,
-    angle_start=-90,
-    angle_end=110,
-    angle_interval=20,
-):
-    """
-    Args:
-            x (float): x coordinate of the nav target (in meters) specified in the world frame
-            y (float): y coordinate of the nav target (in meters) specified in the world frame
-            theta (float): yaw for the nav target (in radians) specified in the world frame
-            object_target: str object to search
-            image_search : spot_rl.utils.heuristic_nav.ImageSearch, Optional, default=None, ImageSearch (object detector wrapper), if none creates a new one for you uses OwlVit
-            save_cone_search_images: bool, optional, default= True, saves image with detections in each search cone
-            pull_back : bool, optional, default=True, pulls back x,y along theta direction
-            skill_manager: skill_manager object to perform low level skills
-        Returns:
-            bool: True if navigation was successful, False otherwise, if True you are good to fire .pick metho
-
-    """
-    if save_cone_search_images:
-        previously_saved_images = glob("imagesearch*.png")
-        for f in previously_saved_images:
-            os.remove(f)
-
-    if image_search is None:
-        image_search = ImageSearch(
-            corner_static_offset=0.5,
-            use_yolov8=False,
-            visualize=save_cone_search_images,
-            version=2,
-        )
-
-    (x, y) = (
-        pull_back_point_along_theta_by_offset(x, y, theta, 0.2) if pull_back else (x, y)
-    )
-    # print(f"Nav targets adjusted on the theta direction ray {x, y, np.degrees(theta)}")
-    # skillmanager.nav(x, y, theta)
-    # skillmanager.nav_controller.nav_env.enable_nav_by_hand()
-
-    spot: Spot = skillmanager.spot
-    spot.open_gripper()
-    gaze_arm_angles = deepcopy(skillmanager.pick_config.GAZE_ARM_JOINT_ANGLES)
-    spot.set_arm_joint_positions(np.deg2rad(gaze_arm_angles), 1)
-    time.sleep(1.5)
-    found, (x, y, theta), point3d_in_vision, visulize_img = image_search.search(
-        object_target, *get_arguments_for_image_search(spot)
-    )
-    rate = angle_interval  # control time taken to rotate the arm, higher the rotation higher is the time
-    if not found:
-        # start semi circle search
-        semicircle_range = np.arange(angle_start, angle_end, angle_interval)
-        for i_a, angle in enumerate(semicircle_range):
-            print(f"Searching in {angle} cone")
-            angle_time = int(np.abs(gaze_arm_angles[0] - angle) / rate)
-            gaze_arm_angles[0] = angle
-            spot.set_arm_joint_positions(np.deg2rad(gaze_arm_angles), angle_time)
-            time.sleep(1.5)
-            (
-                found,
-                (x, y, theta),
-                point3d_in_vision,
-                visulize_img,
-            ) = image_search.search(  # type : ignore
-                object_target, *get_arguments_for_image_search(spot)
-            )
-            if save_cone_search_images:
-                cv2.imwrite(f"imagesearch_{angle}.png", visulize_img)
-            if found:
-                print(f"In Cone Search object found at {(x,y,theta)}")
-                break
-
-    else:
-        if save_cone_search_images:
-            cv2.imwrite("imagesearch_looking_forward.png", visulize_img)
-    angle_time = int(
-        np.abs(gaze_arm_angles[0] - skillmanager.pick_config.GAZE_ARM_JOINT_ANGLES[0])
-        / rate
-    )
-    spot.set_arm_joint_positions(
-        np.deg2rad(skillmanager.pick_config.GAZE_ARM_JOINT_ANGLES),
-        angle_time,
-    )
-    if found:
-        print(f"Nav goal after cone search {x, y, np.degrees(theta)}")
-        point_in_home = np.array([x, y, point3d_in_vision[-1]])
-        # backup_steps = skillmanager.nav_controller.nav_env.max_episode_steps
-        # skillmanager.nav_controller.nav_env.max_episode_steps = 50
-        # skillmanager.nav(x, y, theta)
-        # skillmanager.nav_controller.nav_env.max_episode_steps = backup_steps
-    # skillmanager.nav_controller.nav_env.disable_nav_by_hand()
-    return found, point_in_home, (x, y, theta)
 
 
 def area(x1, y1, x2, y2):
@@ -139,14 +35,14 @@ def select_the_bbox_closer_to_camera(predictions, raw_depth, camera_intrinsics):
         print(i, prediction)
         (x1, y1, x2, y2), score = prediction[0], prediction[-1]
         if x1 < x2 and y1 < y2:
-            if 0 <= x1 <=  w and 0 <= x2 <=  w and 0 <= y1 <=  h and 0 <= y2 <=  h:
-                cx, cy = (x1 + x2) //2, (y1 + y2)//2
+            if 0 <= x1 <= w and 0 <= x2 <= w and 0 <= y1 <= h and 0 <= y2 <= h:
+                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
                 depth_patch = sample_patch_around_point(cx, cy, raw_depth)
                 mean_dist = np.nan_to_num(depth_patch, nan=np.inf)
                 xyz = get_3d_point(camera_intrinsics, (cx, cy), mean_dist)
-                #mean_dist = mean_dist + np.abs(xyz[1]) #y -
+                # mean_dist = mean_dist + np.abs(xyz[1]) #y -
                 print(f"mean dist to prediction {i} is {mean_dist}, {xyz}")
-                if mean_dist <= closer_dist :
+                if mean_dist <= closer_dist:
                     closer_dist = mean_dist
                     closer_i = i
     return predictions[closer_i], closer_dist
@@ -199,6 +95,34 @@ def remove_outliers(arr: np.ndarray):
     return np.array(
         [x for x in arr if (x[0] > (mean - (2 * sd)) and x[0] < (mean + (2 * sd)))]
     )
+
+
+def convert_point_in_body_to_place_waypoint(
+    point_in_body: mn.Vector3, spot: Spot
+):  # 3d point
+    # spotwaypoint recorder method to convert xyz point from hand to global frame
+    position, rotation = spot.get_base_transform_to(
+        "link_wr1"
+    )  # body_T_linkwr1 center wr1 in body
+    position = [point_in_body.x, point_in_body.y, point_in_body.z]
+    rotation = [rotation.x, rotation.y, rotation.z, rotation.w]
+    wrist_T_base = SpotBaseEnv.spot2habitat_transform(position, rotation)
+
+    base_place_target_habitat = np.array(wrist_T_base.translation)
+    base_place_target = base_place_target_habitat[[0, 2, 1]]
+
+    # TODO: Check if we are missing a multiplication with (-1) on y
+
+    x, y, yaw = spot.get_xy_yaw()
+    base_T_global = mn.Matrix4.from_(
+        mn.Matrix4.rotation_z(mn.Rad(yaw)).rotation(),
+        mn.Vector3(mn.Vector3(x, y, 0.5)),
+    )
+    global_place_target = base_T_global.transform_point(base_place_target)
+    global_place_target = np.array(
+        [global_place_target.x, global_place_target.y, global_place_target.z]
+    )
+    return global_place_target
 
 
 def search_table(
@@ -327,6 +251,66 @@ def project_3d_to_pixel_uv(points_3d, fx, fy, cx, cy):
     return np.stack([u.flatten(), v.flatten()], axis=1).reshape(-1, 2)
 
 
+def generate_point_cloud(
+    image, depth, mask, bbox, fx=383.883, fy=383.883, cx=324.092, cy=238.042
+):
+    """
+    Generate a point cloud from an RGB image, depth image, and bounding box.
+
+    Parameters:
+    - rgb_image: RGB image as a numpy array.
+    - depth_image: Depth image as a numpy array.
+    - bbox: Bounding box as a tuple (x_min, y_min, x_max, y_max).
+    - fx, fy: Focal lengths of the camera in x and y dimensions.
+    - cx, cy: Optical center of the camera in x and y dimensions.
+
+    Returns:
+    - point_cloud: Open3D point cloud object.
+    """
+    # depth_image /= depth_image.max()
+    # x_min, y_min, x_max, y_max = bbox
+    # fx, fy, cx, cy = # provide your values for fx, fy, cx, cy
+
+    # Get the indices where mask is True
+
+    x1, y1, x2, y2 = bbox
+    rows, cols = np.where(mask)
+
+    # depth[y1:y2, x1:x2] = inpaint_depth_map(depth[y1:y2, x1:x2])
+
+    # Get the depth values at these indices
+    depth_values = depth[rows, cols]
+
+    depth_values = depth_values.astype(np.float32)
+    depth_values /= 1000.0
+
+    # assert np.any(depth_values == 0), "depth must not be zero"
+    # depth_values[...] = np.mean(depth_values)
+    # Compute the 3D coordinates
+    X = (cols - cx) * depth_values / fx
+    Y = (rows - cy) * depth_values / fy
+    Z = depth_values
+
+    # Combine the X, Y, Z coordinates into a single N x 3 array
+    points_3D = np.vstack((X, Y, Z)).T
+
+    # Optional: Filter out points with a depth of 0 or below a certain threshold
+    valid_depth_indices = depth_values > 0  # Example threshold
+    print(
+        f"Total vertices: {len(points_3D)}, Corrupt vertices: {len(points_3D) - len(valid_depth_indices)}"
+    )
+    points_3D = points_3D[valid_depth_indices]
+
+    print(f"3D point cloud shape {points_3D.shape}")
+    colors = image[rows, cols].reshape(-1, 3) / 255.0
+    colors = colors[valid_depth_indices]
+    point_cloud = o3d.geometry.PointCloud()
+    point_cloud.points = o3d.utility.Vector3dVector(points_3D)
+    point_cloud.colors = o3d.utility.Vector3dVector(colors)
+
+    return point_cloud
+
+
 def get_target_points_by_heuristic(
     np_points,
     x_percentile=50,
@@ -377,7 +361,7 @@ def get_target_points_by_heuristic(
 
 
 def plot_intel_point_in_gripper_image(
-    gripper_image_resps, gripper_T_intel:mn.Matrix4, point3d_in_intel: np.ndarray
+    gripper_image_resps, gripper_T_intel: mn.Matrix4, point3d_in_intel: np.ndarray
 ):
     gripper_intrinsics = gripper_image_resps[0].source.pinhole.intrinsics
     gripper_image: np.ndarray = [
@@ -386,9 +370,13 @@ def plot_intel_point_in_gripper_image(
     ]
     gripper_image, gripper_depth = gripper_image[0], gripper_image[1]
     # breakpoint()
-    point3d_in_gripper: mn.Matrix3 = gripper_T_intel.transform_point(mn.Vector3(point3d_in_intel)) 
-    point3d_in_gripper:np.ndarray = np.array([point3d_in_gripper.x, point3d_in_gripper.y, point3d_in_gripper.z])
-    
+    point3d_in_gripper: mn.Matrix3 = gripper_T_intel.transform_point(
+        mn.Vector3(point3d_in_intel)
+    )
+    point3d_in_gripper: np.ndarray = np.array(
+        [point3d_in_gripper.x, point3d_in_gripper.y, point3d_in_gripper.z]
+    )
+
     fx = gripper_intrinsics.focal_length.x
     fy = gripper_intrinsics.focal_length.y
     cx = gripper_intrinsics.principal_point.x
@@ -402,60 +390,31 @@ def plot_intel_point_in_gripper_image(
         2,
         (0, 0, 255),
     )
-    #print(f"Point in gripper {point3d_in_gripper}, depth in gripper {gripper_depth[int(point2d_in_gripper[1]), int(point2d_in_gripper[0])]/1000.}")
+    # print(f"Point in gripper {point3d_in_gripper}, depth in gripper {gripper_depth[int(point2d_in_gripper[1]), int(point2d_in_gripper[0])]/1000.}")
     return img_with_point
 
 
 def detect_place_point_by_pcd_method(
     img,
     depth_raw,
-    gripper_depth,
     camera_intrinsics_intel,
     camera_intrinsics_gripper,
     body_T_hand: mn.Matrix4,
-    gripper_T_intel:mn.Matrix4,
-    object_name: str = "table top",
-    percentile=95,
-    owlvitmodel=None,
-    proceesor=None,
-    sammodel=None,
-    device="cpu",
+    gripper_T_intel: mn.Matrix4,
+    percentile: float = 70,
 ):
-    # if owlvitmodel is None and proceesor is None:
-    #     owlvitmodel, proceesor = load_model("owlvit", device)
-
+    """
+    depth_raw is intel_raw_depth
+    percentile denotes threshold on width of the receptacle
+    """
     h, w = img.shape[:2]
-    predictions = detect_with_socket(img, object_name, 0.01, device)
-    # predictions = detect(img, object_name, 0.01, device, owlvitmodel, proceesor)
-    prediction, distance_to_prediction = select_the_bbox_closer_to_camera(
-        predictions, depth_raw, camera_intrinsics_intel
-    )
-    if distance_to_prediction > 2000:
-        return owlvitmodel, proceesor, sammodel, None, None
-    img_with_bbox = img.copy()
-    for bbox in predictions:
-        x1, y1, x2, y2 = bbox[0]
-        score = bbox[-1]
-        img_with_bbox = cv2.rectangle(img_with_bbox, (x1, y1), (x2, y2), (255, 0, 0), 1)
-        img_with_bbox = cv2.putText(
-            img_with_bbox,
-            f"{score:.2f}, {area(x1, y1, x2, y2)}",
-            (x1, y1),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=0.9,
-            color=(0, 0, 255),
-            thickness=2,
-        )
-    #cv2.imshow("bbox", img_with_bbox)
-    #cv2.waitKey(0)
     mask = np.ones_like(depth_raw).astype(bool)
-    mask[y1:y2, x1:x2] = True
     fx = camera_intrinsics_intel.focal_length.x
     fy = camera_intrinsics_intel.focal_length.y
     cx = camera_intrinsics_intel.principal_point.x
     cy = camera_intrinsics_intel.principal_point.y
-    #u,v in pixel -> depth at u,v, intriniscs -> xyz in 3D
-    pcd = generate_point_cloud(img, depth_raw, mask, prediction[0], fx, fy, cx, cy)
+    # u,v in pixel -> depth at u,v, intriniscs -> xyz in 3D
+    pcd = generate_point_cloud(img, depth_raw, mask, [0, 0, 0, 0], fx, fy, cx, cy)
     # o3d.io.write_point_cloud("original_pcd.ply", pcd)
     plane_pcd = plane_detect(pcd)
     # DownSample
@@ -471,8 +430,8 @@ def detect_place_point_by_pcd_method(
         np.array(plane_pcd.points)
     )
     corners_xys = project_3d_to_pixel_uv(target_points, fx, fy, cx, cy)
-    #print(corners_xys)
-    y_threshold = np.percentile(corners_xys[:, -1], 70)
+    # print(corners_xys)
+    y_threshold = np.percentile(corners_xys[:, -1], percentile)
 
     # breakpoint()
     indices_gtr_thn_y_thresh = corners_xys[:, 1] >= y_threshold
@@ -487,28 +446,49 @@ def detect_place_point_by_pcd_method(
         / 1000.0
     )
     print(f"Depth in Intel {depth_at_selected_xy}")
-    #depth_at_selected_xy = depth_raw[int(selected_xy[-1]), int(selected_xy[0])] / 1000.0
+    # depth_at_selected_xy = depth_raw[int(selected_xy[-1]), int(selected_xy[0])] / 1000.0
     assert (
         depth_at_selected_xy != 0.0
     ), f"Non zero depth required found {depth_at_selected_xy}"
-    selected_point = get_3d_point(camera_intrinsics_intel, selected_xy, depth_at_selected_xy)
-    #print(f"Point in Intel {selected_point}")
-    
-    #Convert selected point in gripper 3D to 3D then 3D to 2D then 2D to 3D using depth at Gripper
-    #Ideally body-T_hand*hand_T_intel*point_in_intel should work but hand_T_intel has errors plus depth in Intel at closer points is not same as gripper 
-    #thus we convert intel 3D point to Gripper 3D, convert 3D to 2D & then again from 2D to 3D using depth map
-    selected_point_in_gripper = gripper_T_intel.transform_point(mn.Vector3(*selected_point))
-    selected_point_in_gripper = np.array([selected_point_in_gripper.x, selected_point_in_gripper.y, selected_point_in_gripper.z])
+    selected_point = get_3d_point(
+        camera_intrinsics_intel, selected_xy, depth_at_selected_xy
+    )
+    # print(f"Point in Intel {selected_point}")
+
+    # Convert selected point in gripper 3D to 3D then 3D to 2D then 2D to 3D using depth at Gripper
+    # Ideally body-T_hand*hand_T_intel*point_in_intel should work but hand_T_intel has errors plus depth in Intel at closer points is not same as gripper
+    # thus we convert intel 3D point to Gripper 3D, convert 3D to 2D & then again from 2D to 3D using depth map
+    selected_point_in_gripper = gripper_T_intel.transform_point(
+        mn.Vector3(*selected_point)
+    )
+    selected_point_in_gripper = np.array(
+        [
+            selected_point_in_gripper.x,
+            selected_point_in_gripper.y,
+            selected_point_in_gripper.z,
+        ]
+    )
     fx = camera_intrinsics_gripper.focal_length.x
     fy = camera_intrinsics_gripper.focal_length.y
     cx = camera_intrinsics_gripper.principal_point.x
     cy = camera_intrinsics_gripper.principal_point.y
-    selected_xy_in_gripper = project_3d_to_pixel_uv(selected_point_in_gripper.reshape(1, 3), fx, fy, cx, cy)[0]
-    depth_at_selected_xy_in_gripper = depth_at_selected_xy + 0.02 #Add 2 cm in intel depth to get depth in gripper
+    selected_xy_in_gripper = project_3d_to_pixel_uv(
+        selected_point_in_gripper.reshape(1, 3), fx, fy, cx, cy
+    )[0]
+    depth_at_selected_xy_in_gripper = (
+        depth_at_selected_xy + 0.02
+    )  # Add 2 cm in intel depth to get depth in gripper
     print(f"Depth in gripper {depth_at_selected_xy_in_gripper}")
-    assert depth_at_selected_xy_in_gripper != 0., f"Expeceted gripper depth at point {int(selected_xy_in_gripper[1]), int(selected_xy_in_gripper[0])} to be non zero but found {depth_at_selected_xy_in_gripper}"
-    selected_point_in_gripper = get_3d_point(camera_intrinsics_gripper, selected_xy_in_gripper, depth_at_selected_xy_in_gripper)
-    
+    assert (
+        depth_at_selected_xy_in_gripper != 0.0
+    ), f"Expeceted gripper depth at point {int(selected_xy_in_gripper[1]), int(selected_xy_in_gripper[0])} to be non zero but found {depth_at_selected_xy_in_gripper}"
+    selected_point_in_gripper = get_3d_point(
+        camera_intrinsics_gripper,
+        selected_xy_in_gripper,
+        depth_at_selected_xy_in_gripper,
+    )
+
+    img_with_bbox = img.copy()
     for xy in corners_xys:
         img_with_bbox = cv2.circle(
             img_with_bbox, (int(xy[0]), int(xy[1])), 1, (255, 0, 0)
@@ -516,18 +496,22 @@ def detect_place_point_by_pcd_method(
     img_with_bbox = cv2.circle(
         img_with_bbox, (int(selected_xy[0]), int(selected_xy[1])), 2, (0, 0, 255)
     )
-    breakpoint()
+    # breakpoint()
     cv2.imshow("Table detection", img_with_bbox)
     cv2.waitKey(0)
-    #o3d.visualization.draw_geometries([pcd, plane_pcd])
+    # o3d.visualization.draw_geometries([pcd, plane_pcd])
     cv2.imwrite("table_detection.png", img_with_bbox)
     cv2.destroyAllWindows()
     # o3d.io.write_point_cloud("plane_pcd.ply", plane_pcd)
-    point_in_body = np.array(body_T_hand.transform_point(mn.Vector3(*selected_point_in_gripper)))
-    #point_in_vision_with_direct_transform = body_T_hand.transform_point(gripper_T_intel.transform_point(mn.Vector3(selected_point)))
-    #print(f"Point in body with direct transform {point_in_vision_with_direct_transform}, without direct transform {point_in_vision}")
-    return owlvitmodel, proceesor, None, point_in_body, selected_point_in_gripper, img_with_bbox
-#detected object intel RS 2D- > 3D -3D gripper- > 2D in gripper 
-#3D intel - 3D gripper problem in z coordinate
-#Intel point -> spot 
-#3D point in Intel - 3D point gripper -> 2D point in gripper + Depth at Intel with offset -> 3D point in gripper
+    point_in_body = np.array(
+        body_T_hand.transform_point(mn.Vector3(*selected_point_in_gripper))
+    )
+    # point_in_vision_with_direct_transform = body_T_hand.transform_point(gripper_T_intel.transform_point(mn.Vector3(selected_point)))
+    # print(f"Point in body with direct transform {point_in_vision_with_direct_transform}, without direct transform {point_in_vision}")
+    return point_in_body, selected_point_in_gripper, img_with_bbox
+
+
+# detected object intel RS 2D- > 3D -3D gripper- > 2D in gripper
+# 3D intel - 3D gripper problem in z coordinate
+# Intel point -> spot
+# 3D point in Intel - 3D point gripper -> 2D point in gripper + Depth at Intel with offset -> 3D point in gripper
