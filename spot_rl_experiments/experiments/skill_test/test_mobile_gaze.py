a3eeee9e1d9cabfb2abb1ac63d544675213c0140
Jimmy Yang <yangtsungyen@gmail.com>
2024-04-10 11:07:42 -0400

    load the newly trained checkpoints

--- spot_rl_experiments/experiments/skill_test/test_mobile_gaze.py
+++ spot_rl_experiments/experiments/skill_test/test_mobile_gaze.py
@@ -9,25 +9,10 @@ from spot_rl.envs.skill_manager import SpotSkillManager
 if __name__ == "__main__":
     from perception_and_utils.utils.generic_utils import map_user_input_to_boolean
 
-    # Pick Targets from Aria with +- 10 degree angle change
-    pick_targets = [
-        "kitchen",
-        "kitchen_-10",
-        "kitchen_+10",
-        "console",
-        "console_+15",
-        "console_-15",
-    ]
+    spotskillmanager = SpotSkillManager(use_mobile_pick=True)
     contnue = True
-    i: int = 0
-    n: int = len(pick_targets)
-    while contnue and i < 3:
-        pick_from = pick_targets[i]
-        i += 1
-        spotskillmanager = SpotSkillManager(use_mobile_pick=True)
-        spotskillmanager.nav(pick_from)
-        x = input(f"Press Enter to continue to mobile gaze from {pick_from}")
-        spotskillmanager.pick("creal_box")
+    while contnue:
+        spotskillmanager.pick("cup")
         spotskillmanager.spot.open_gripper()
         contnue = map_user_input_to_boolean("Do you want to do it again ? Y/N ")
 
