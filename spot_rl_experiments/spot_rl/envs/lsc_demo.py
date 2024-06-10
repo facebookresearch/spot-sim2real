2d1a2d7f8c1a7a07313655dae011fa288d0caa13
Tushar Sangam <tusharsangam5@gmail.com>
2024-06-10 15:30:01 -0700

    keep_alive change

--- spot_rl_experiments/spot_rl/envs/lsc_demo.py
+++ spot_rl_experiments/spot_rl/envs/lsc_demo.py
@@ -48,30 +48,29 @@ def main(spot, config):
     while True:
         audio_transcription_success = False
         while not audio_transcription_success:
-            try:
-                input("Are you ready?")
-                audio_to_text.record()
-                instruction = audio_to_text.translate()
-                print("Transcribed instructions : ", instruction)
+            # try:
+            input("Are you ready?")
+            audio_to_text.record()
+            instruction = audio_to_text.translate()
+            print("Transcribed instructions : ", instruction)
 
-                # Use LLM to convert user input to an instructions set
-                # Eg: nav_1, pick, nav_2 = 'bowl_counter', "container", 'coffee_counter'
-                nav_1, pick, nav_2, _ = llm.parse_instructions(instruction)
-                print("PARSED", nav_1, pick, nav_2)
+            # Use LLM to convert user input to an instructions set
+            # Eg: nav_1, pick, nav_2 = 'bowl_counter', "container", 'coffee_counter'
+            t1 = time.time()
+            nav_1, pick, nav_2, _ = llm.parse_instructions(instruction)
+            print("PARSED", nav_1, pick, nav_2, f"Time {time.time()- t1} secs")
 
-                # Find closest nav_targets to the ones robot knows locations of
-                nav_1 = sentence_similarity.get_most_similar_in_list(
-                    nav_1, list(waypoints_yaml_dict["nav_targets"].keys())
-                )
-                nav_2 = sentence_similarity.get_most_similar_in_list(
-                    nav_2, list(waypoints_yaml_dict["nav_targets"].keys())
-                )
-                print("MOST SIMILAR: ", nav_1, pick, nav_2)
-                audio_transcription_success = True
-            except Exception as e:
-                print(f"Exception encountered in Speech to text : {e} \n\n Retrying...")
-
-        spot = SpotSkillManager(use_mobile_pick=True) if spot is None else spot
+            # Find closest nav_targets to the ones robot knows locations of
+            nav_1 = sentence_similarity.get_most_similar_in_list(
+                nav_1, list(waypoints_yaml_dict["nav_targets"].keys())
+            )
+            nav_2 = sentence_similarity.get_most_similar_in_list(
+                nav_2, list(waypoints_yaml_dict["nav_targets"].keys())
+            )
+            print("MOST SIMILAR: ", nav_1, pick, nav_2)
+            audio_transcription_success = True
+            # except Exception as e:
+            #     print(f"Exception encountered in Speech to text : {e} \n\n Retrying...")
         # Used for Owlvit
         rospy.set_param("object_target", pick)
 
@@ -80,6 +79,7 @@ def main(spot, config):
         rospy.set_param("viz_object", pick)
         rospy.set_param("viz_place", nav_2)
 
+        spot = SpotSkillManager(use_mobile_pick=True) if spot is None else spot
         if pre_in_dock:
             spot.spot.power_robot()
 
--- spot_rl_experiments/spot_rl/llm/src/llama3_local_api.py
+++ spot_rl_experiments/spot_rl/llm/src/llama3_local_api.py
@@ -5,26 +5,43 @@ import requests
 
 # LLAMA3 is supposed to be working locally
 # Run OLLAMA3 as ollama run llama3
-model_name = "llama3:70b"
+
+template_prompt = "You will solve a simple rearrangement task that requires you to Navigate to a given object\n and Pick it up, and then Navigate to a given location and Place it there. Given an open instruction\n that could be similar to Go to the table and find the mug, and return the mug to box, you need to \n return the solution sequence of actions: Nav(table), Pick(mug), Nav(box), Place(mug, box).\n EXAMPLES:\n Instruction: Go to table and find the mug, and return the mug to box \n Solution: Nav(table), Pick(mug), Nav(box), Place(mug, box) \n Instruction: Bring the apple from the kitchen counter to the table \n Solution: Nav(kitchen counter), Pick(apple), Nav(table), Place(apple, table) \n Lets go! \n Instruction: Hey Spot, can you pick up the ball from the couch and place it on the round table? \n Solution:"
 
 
 class MetaAI:
-    def __init__(self):
-        self.url = "http://localhost:11434/api/generate"
+    def __init__(self, model_name="llama3"):
+        self.url = "http://localhost:11434/api/"
+        self.model_name = model_name
+
+        self.data = {
+            "model": self.model_name,
+            "keep_alive": -1,
+            "options": {"main_gpu": 1},
+            "prompt": template_prompt,
+            "stream": False,
+        }
+        response = requests.post(self.url + "generate", json=self.data)
+        status_code = response.status_code
+        response = json.loads(response.text)
+        self.context = response.get("context", [])
+        assert (
+            status_code == 200
+        ), f"ollama returned status code {status_code}, failed to start ollama with {self.model_name}"
 
     def prompt(self, message: str = "Hey, are you LLAMA3 ?") -> Dict[str, str]:
-        data = {"model": model_name, "prompt": message, "stream": False}
-        response = requests.post(self.url, json=data, timeout=30)
+        self.data.update({"prompt": message, "context": self.context})
+        response = requests.post(self.url + "generate", json=self.data)
         status_code = response.status_code
         assert (
             status_code == 200
         ), f"ollama returned status code {status_code}, please check if ollama is running correctly"
         respons = json.loads(response.text)
+        self.context = respons.get("context", [])
         return {"message": str(respons["response"])}
 
 
 if __name__ == "__main__":
     ai = MetaAI()
-    prompt = "You will solve a simple rearrangement task that requires you to Navigate to a given object\n and Pick it up, and then Navigate to a given location and Place it there. Given an open instruction\n that could be similar to Go to the table and find the mug, and return the mug to box, you need to \n return the solution sequence of actions: Nav(table), Pick(mug), Nav(box), Place(mug, box).\n EXAMPLES:\n Instruction: Go to table and find the mug, and return the mug to box \n Solution: Nav(table), Pick(mug), Nav(box), Place(mug, box) \n Instruction: Bring the apple from the kitchen counter to the table \n Solution: Nav(kitchen counter), Pick(apple), Nav(table), Place(apple, table) \n Lets go! \n Instruction: Hey Spot, can you pick up the ball from the couch and place it on the round table? \n Solution:"
-    response = ai.prompt(message=prompt)
+    response = ai.prompt(message=template_prompt)
     print(response)
