4256b474f4abd0eb55a3d44ea0504595887c4422
Tushar Sangam <tusharsangam5@gmail.com>
2024-08-16 18:30:34 -0700

    ,taking backup

--- spot_rl_experiments/spot_rl/utils/collision_detector.py
+++ spot_rl_experiments/spot_rl/utils/collision_detector.py
@@ -1,8 +1,14 @@
+import atexit
+
 import matplotlib.pyplot as plt
 import numpy as np
+import rospy
 from matplotlib.animation import FuncAnimation
 
 spot = None
+recorded_data = None
+save_path: str = "joint_states_recording.npy"
+len_of_torque_data: int = 0
 
 
 def get_arm_joints_from_spot(rename=True):
@@ -25,13 +31,26 @@ def get_arm_joints_from_spot(rename=True):
     return arm_joint_names, arm_torque_values
 
 
+def dump_recorded_data():
+    global recorded_data
+    global save_path
+    if recorded_data:
+        torques = np.array(list(recorded_data.values()), dtype=np.float32).reshape(
+            8, -1
+        )
+        np.save(save_path, torques)
+        print(f"Data saved at {save_path}")
+
+
+atexit.register(dump_recorded_data)
+
 if __name__ == "__main__":
     from spot_wrapper.spot import Spot
 
     spot = Spot("arm collision detector")
 
     # Create the figure and axis objects
-    fig, axes = plt.subplots(nrows=8, ncols=1, figsize=(12, 12))
+    fig, axes = plt.subplots(nrows=9, ncols=1, figsize=(12, 12))
 
     # Initialize the plot with some default data
     joint_names, torques = get_arm_joints_from_spot(False)
@@ -52,20 +71,31 @@ if __name__ == "__main__":
     # Function to update the plot
     def update(frame):
         # Plot the instant torque
+        global recorded_data
+        global len_of_torque_data
+
         _, torques = get_arm_joints_from_spot(False)
+        is_recording_on: bool = rospy.get_param("record_joints", False)
         for bar, torque in zip(bars, torques):
             bar.set_height(torque)
 
         # Plot the history of the troque
-        for i in range(7):
+        for i in range(len(torques)):
             axes[i + 1].cla()
+            if len_of_torque_data > max_len:
+                torque_data[i] = []
+                len_of_torque_data = 0
             torque_data[i].append(torques[i])
-            torque_data[i] = torque_data[i][-max_len:]
+            len_of_torque_data += 1
+            # torque_data[i] = torque_data[i][-max_len:]
             index = [i for i in range(len(torque_data[i]))]
             axes[i + 1].plot(index, torque_data[i])
             axes[i + 1].set_xlim([0, max_len])
 
         fig.tight_layout()
+
+        if is_recording_on:
+            recorded_data = torque_data
 
     # Create an animation
     ani = FuncAnimation(fig, update, frames=range(100), blit=False, interval=100)
