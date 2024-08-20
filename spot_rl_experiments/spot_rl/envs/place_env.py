3ed8dbaa9c8542816a6a9baff5638cab17f7b907
Joanne Truong <jtruong@meta.com>
2024-08-20 13:44:03 -0700

    initial cleanup for place env

--- spot_rl_experiments/spot_rl/envs/place_env.py
+++ spot_rl_experiments/spot_rl/envs/place_env.py
@@ -25,6 +25,8 @@ class SpotPlaceEnv(SpotBaseEnv):
 
         self.ee_gripper_offset = mn.Vector3(config.EE_GRIPPER_OFFSET)
         self.placed = False
+        self.max_episode_steps = config.MAX_PLACE_EPISODE_STEPS
+        self.num_steps = 0
 
     def reset(self, place_target, target_is_local=False, *args, **kwargs):
         assert place_target is not None
@@ -32,26 +34,38 @@ class SpotPlaceEnv(SpotBaseEnv):
         self.place_target_is_local = target_is_local
 
         self.reset_arm()
+        # We wait for a second to let the arm in the placing
+        # ready location
+        time.sleep(1.0)
 
         observations = super().reset()
         self.placed = False
         return observations
 
     def step(self, action_dict: Dict[str, Any], *args, **kwargs):
-        gripper_pos_in_base_frame = self.get_gripper_position_in_base_frame_hab()
-        place_target_in_base_frame = self.get_base_frame_place_target_hab()
-        place = is_position_within_bounds(
-            gripper_pos_in_base_frame,
-            place_target_in_base_frame,
-            self.config.SUCC_XY_DIST,
-            self.config.SUCC_Z_DIST,
-            convention="habitat",
-        )
+        place = self.get_heuristic_place()
 
         # Update the action_dict with place flag
         action_dict["place"] = place
 
         return super().step(action_dict=action_dict, *args, **kwargs)
+
+    def get_heuristic_place(self, convention="habitat"):
+        if convention == "habitat":
+            gripper_pos_in_base_frame = self.get_gripper_position_in_base_frame_hab()
+            place_target_in_base_frame = self.get_base_frame_place_target_hab()
+        elif convention == "spot":
+            gripper_pos_in_base_frame = self.get_gripper_position_in_base_frame_spot()
+            place_target_in_base_frame = self.get_base_frame_place_target_spot()
+
+        place = is_position_within_bounds(
+            gripper_pos_in_base_frame,
+            place_target_in_base_frame,
+            self.config.SUCC_XY_DIST,
+            self.config.SUCC_Z_DIST,
+            convention=convention,
+        )
+        return place
 
     def get_success(self, observations):
         return self.place_attempted
@@ -65,8 +79,8 @@ class SpotPlaceEnv(SpotBaseEnv):
         return observations
 
 
-class SpotSemanticPlaceEnv(SpotBaseEnv):
-    """This is Spot semantic place class"""
+class SpotSemanticPlaceEnv(SpotPlaceEnv):
+    """This is Spot semantic place (with joint control) class"""
 
     def __init__(self, config, spot: Spot):
         # We set the initial arm joints
@@ -74,14 +88,6 @@ class SpotSemanticPlaceEnv(SpotBaseEnv):
             config.INITIAL_ARM_JOINT_ANGLES_SEMANTIC_PLACE
         )
         super().__init__(config, spot)
-
-        # Define the place variables
-        self.place_target = None
-        self.place_target_is_local = False
-        self.placed = False
-
-        # Set gripper variables
-        self.ee_gripper_offset = mn.Vector3(config.EE_GRIPPER_OFFSET)
 
         # Define the observation variables
         self.initial_ee_pose = None
@@ -91,8 +97,7 @@ class SpotSemanticPlaceEnv(SpotBaseEnv):
         self.arm_lower_limits = np.deg2rad(config.ARM_LOWER_LIMITS_SEMANTIC_PLACE)
         self.arm_upper_limits = np.deg2rad(config.ARM_UPPER_LIMITS_SEMANTIC_PLACE)
 
-        # Place steps
-        self._time_step = 0
+        self.travel_time_scale = 1.0 / 0.9 * 1.75
 
     def decide_init_arm_joint(self, ee_orientation_at_grasping):
         """Decide the place location"""
@@ -107,7 +112,7 @@ class SpotSemanticPlaceEnv(SpotBaseEnv):
             pitch = ee_orientation_at_grasping[1]
             yaw = ee_orientation_at_grasping[2]
             print("ee_orientation_at_grasping:", ee_orientation_at_grasping)
-            if abs(pitch) <= 1.309:  # 75 degree in pitch
+            if abs(pitch) <= np.deg2rad(75):
                 if yaw > 0:  # gripper is in object's right hand side
                     self.initial_arm_joint_angles = np.deg2rad(
                         self.config.INITIAL_ARM_JOINT_ANGLES_SEMANTIC_PLACE
@@ -122,59 +127,33 @@ class SpotSemanticPlaceEnv(SpotBaseEnv):
                 )
 
     def reset(self, place_target, target_is_local=False, *args, **kwargs):
-        assert place_target is not None
-        self.place_target = np.array(place_target)
-        self.place_target_is_local = target_is_local
-
-        self._time_step = 0
-
-        # Decide the reset arm angle and then reset the arm
         self.decide_init_arm_joint(kwargs["ee_orientation_at_grasping"])
-        self.reset_arm()
-
-        # We wait for a second to let the arm in the placing
-        # ready location
-        time.sleep(1.0)
-        # Sometimes, there will be a bit of mistchmatch of joints after resetting
-        # So we can reset the arm again here using the following
-        # ee_position, ee_orientation = self.spot.get_ee_pos_in_body_frame()
-        # self.spot.move_gripper_to_point(ee_position, [np.pi / 2, 0, 0])
-
+        observations = super().reset(place_target, target_is_local)
         # Set the initial ee pose
         self.initial_ee_pose = self.spot.get_ee_quaternion_in_body_frame()
         # Set the target pose
         self.target_object_pose = self.spot.get_ee_quaternion_in_body_frame()
         # Automatically use intelrealsense camera
         rospy.set_param("is_gripper_blocked", 1)
-        observations = super().reset()
         rospy.set_param("is_whiten_black", False)
-        self.placed = False
         return observations
 
+    def get_heuristic_place(self, convention):
+        super().get_heuristic_place(convention)
+
+        # If the time steps exceeds max_episode_steps, we will just place the object
+        if self.num_steps >= self.max_episode_steps:
+            place = True
+
+        return place
+
     def step(self, action_dict: Dict[str, Any], *args, **kwargs):
-
-        place = False
-
         # Place command is issued if the place action is smaller than zero
-        place = action_dict.get("grip_action", None) <= 0.0
+        place = action_dict.get("grip_action", 1.0) <= 0.0
 
-        # If the time steps have been passed for 50 steps and gripper is in the desired place location
-        cur_place_sensor_xyz = self.get_place_sensor(True)
-        if (
-            abs(cur_place_sensor_xyz[2]) < 0.05
-            and np.linalg.norm(
-                np.array([cur_place_sensor_xyz[0], cur_place_sensor_xyz[1]])
-            )
-            < 0.25
-            and self._time_step >= 50
-        ):
-            place = True
+        place = self.get_heuristic_place()
 
-        # If the time steps have been passed for 75 steps, we will just place the object
-        if self._time_step >= 75:
-            place = True
-
-        self._time_step += 1
+        self.num_steps += 1
 
         # Write into action dict
         action_dict["place"] = place
@@ -182,17 +161,14 @@ class SpotSemanticPlaceEnv(SpotBaseEnv):
 
         # Set the travel time scale so that the arm movement is smooth
         return super().step(
-            action_dict, travel_time_scale=1.0 / 0.9 * 1.75, *args, **kwargs
+            action_dict, travel_time_scale=self.travel_time_scale, *args, **kwargs
         )
-
-    def get_success(self, observations):
-        return self.place_attempted
 
     def get_observations(self):
         assert self.initial_ee_pose is not None
         assert self.target_object_pose is not None
 
-        # Get the gaol sensor
+        # Get the goal sensor
         obj_goal_sensor = self.get_place_sensor(True)
 
         # Get the delta ee orientation
@@ -219,4 +195,18 @@ class SpotSemanticPlaceEnv(SpotBaseEnv):
             "joint": self.get_arm_joints(self.config.SEMANTIC_PLACE_JOINT_BLACKLIST),
             "is_holding": np.ones((1,)),
         }
+        return observations
+
+class SpotSemanticPlaceEEEnv(SpotSemanticPlaceEnv):
+    """This is Spot semantic place (with end-effector control) class"""
+
+    def __init__(self, config, spot: Spot):
+        super().__init__(config, spot)
+
+    def get_observations(self):
+        observations = super().get_observations()
+        if "joint" in observations:
+            del observations["joint"]
+        xyz, _ = self.spot.get_ee_pos_in_body_frame()
+        observations["ee_pos"] = xyz
         return observations
