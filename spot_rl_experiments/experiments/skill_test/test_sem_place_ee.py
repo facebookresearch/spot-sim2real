c49d8704ad633faa112e3b7a41bd7211377cb0a5
Joanne Truong <jtruong@meta.com>
2024-08-23 12:25:19 -0400

    add logging and open gripper to semantic place ee test

--- spot_rl_experiments/experiments/skill_test/test_sem_place_ee.py
+++ spot_rl_experiments/experiments/skill_test/test_sem_place_ee.py
@@ -4,6 +4,8 @@
 import rospy
 from perception_and_utils.utils.generic_utils import map_user_input_to_boolean
 from spot_rl.envs.skill_manager import SpotSkillManager
+from datetime import datetime
+import json
 
 if __name__ == "__main__":
     # Know which location we are doing experiments
@@ -23,7 +25,22 @@ if __name__ == "__main__":
 
     # Start testing
     contnue = True
+    episode_ctr = 0
     while contnue:
+        spotskillmanager.spot.open_gripper()
+        input('Place an object in Spot\'s gripper and press Enter to continue...')
         rospy.set_param("is_gripper_blocked", 0)
+        episode_log = {"actions": []}
+
         spotskillmanager.place(place_target, is_local=is_local, visualize=False)
+        skill_log = spotskillmanager.place_controller.skill_result_log
+        if "num_steps" not in skill_log:
+            skill_log["num_steps"] = 0
+        episode_log["actions"].append({f"place": skill_log})
+        curr_date = datetime.today().strftime('%m-%d-%y')
+        file_path = f"logs/semantic_place_ee/{curr_date}/episode_{episode_ctr}.json"
+        with open(file_path, "w") as file:
+            json.dump(episode_log, file, indent=4)
+            print(f"Saved log: {file_path}")
+        episode_ctr +=1
         contnue = map_user_input_to_boolean("Do you want to do it again ? Y/N ")
