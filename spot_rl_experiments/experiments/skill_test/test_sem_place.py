f6200b80304dcc2e94899bf09a0f1d63fda867ab
Achuthankrishna <achkrish07@gmail.com>
2024-09-03 16:26:32 -0700

    Added wapoint estimator flags and cleanup

--- spot_rl_experiments/experiments/skill_test/test_sem_place.py
+++ spot_rl_experiments/experiments/skill_test/test_sem_place.py
@@ -77,19 +77,3 @@ if __name__ == "__main__":
         contnue = map_user_input_to_boolean("Do you want to do it again ? Y/N ")
         # Return the arm to the original position
         spotskillmanager.spot.move_gripper_to_point(spot_pos, spot_ort)
-# The following is a helpful tip to debug the arm
-# We get Spot class
-# spot = spotskillmanager.spot
-# We can move the gripper to a point with x,y,z and roll, pitch, yaw
-# spot.move_gripper_to_point((0.55, 0., 0.26), np.deg2rad(np.array([0,0,0])))
-# We can also set the robot arm joints
-# config = construct_config()
-# spot.set_arm_joint_positions(np.deg2rad(config.INITIAL_ARM_JOINT_ANGLES))
-
-# In addition, if you want to use semantic place skill based on the grasping orientation, you can do
-# spotskillmanager.nav("black_case")
-# spotskillmanager.pick("bottle")
-# # Fetch the arm joint at grasping location
-# ee_orientation_at_grasping = spotskillmanager.gaze_controller.env.ee_orientation_at_grasping
-# spotskillmanager.nav("test_desk")
-# spotskillmanager.place("test_desk", orientation_at_grasping) # This controls the arm initial orientation
