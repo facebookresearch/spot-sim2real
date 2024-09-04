426a2f3217dad71f8024ba9e8c6d87ae23d93190
Achuthankrishna <achkrish07@gmail.com>
2024-09-04 12:28:58 -0700

    minor conflict resolutions

--- spot_rl_experiments/experiments/skill_test/test_sem_place_ee.py
+++ spot_rl_experiments/experiments/skill_test/test_sem_place_ee.py
@@ -1,15 +1,10 @@
 # Copyright (c) Meta Platforms, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-<<<<<<< HEAD
 # mypy: ignore-errors
 import json
 from datetime import datetime
 
-=======
-import json
-from datetime import datetime
->>>>>>> 69d9e3f (Minor chagnges for new policy weights)
 import rospy
 from perception_and_utils.utils.generic_utils import map_user_input_to_boolean
 from spot_rl.envs.skill_manager import SpotSkillManager
@@ -19,11 +14,7 @@ if __name__ == "__main__":
     enable_estimation_before_place = map_user_input_to_boolean(
         "Use waypoint estimator? Y/N "
     )
-<<<<<<< HEAD
     place_target = "fre_lab_40in_sep"
-=======
-    place_target = "coffee_table"
->>>>>>> 69d9e3f (Minor chagnges for new policy weights)
     spotskillmanager = SpotSkillManager(
         use_mobile_pick=False, use_semantic_place=True, use_place_ee=True
     )
@@ -47,21 +38,11 @@ if __name__ == "__main__":
         skill_log = spotskillmanager.place_controller.skill_result_log
         if "num_steps" not in skill_log:
             skill_log["num_steps"] = 0
-<<<<<<< HEAD
         episode_log["actions"].append({"place": skill_log})
         curr_date = datetime.today().strftime("%m-%d-%y")
         file_path = f"logs/semantic_place_ee/{curr_date}/0.5m_minus_20deg_episode_{episode_ctr}.json"
-=======
-        episode_log["actions"].append({f"place": skill_log})
-        curr_date = datetime.today().strftime("%m-%d-%y")
-        file_path = f"logs/semantic_place_ee/{curr_date}/episode_{episode_ctr}.json"
->>>>>>> 69d9e3f (Minor chagnges for new policy weights)
         with open(file_path, "w") as file:
             json.dump(episode_log, file, indent=4)
             print(f"Saved log: {file_path}")
         episode_ctr += 1
-<<<<<<< HEAD
         contnue = map_user_input_to_boolean("Do you want to do it again ? Y/N ")
-=======
-        contnue = map_user_input_to_boolean("Do you want to do it again ? Y/N ")
->>>>>>> 69d9e3f (Minor chagnges for new policy weights)
