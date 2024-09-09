987db16e35d997f2cb440643eaa86bab717a1410
Achuthankrishna <achkrish07@gmail.com>
2024-09-09 10:35:10 -0700

    Removed comments and restored debug method

--- spot_rl_experiments/experiments/skill_test/test_sem_place.py
+++ spot_rl_experiments/experiments/skill_test/test_sem_place.py
@@ -50,7 +50,22 @@ if __name__ == "__main__":
         episode_log = {"actions": []}
         spotskillmanager.spot.close_gripper()
         input("waiting for user to get ready with camera")
+        # The following is a helpful tip to debug the arm
+        # We get Spot class
+        # spot = spotskillmanager.spot
+        # We can move the gripper to a point with x,y,z and roll, pitch, yaw
+        # spot.move_gripper_to_point((0.55, 0., 0.26), np.deg2rad(np.array([0,0,0])))
+        # We can also set the robot arm joints
+        # config = construct_config()
+        # spot.set_arm_joint_positions(np.deg2rad(config.INITIAL_ARM_JOINT_ANGLES))
 
+        # In addition, if you want to use semantic place skill based on the grasping orientation, you can do
+        # spotskillmanager.nav("black_case")
+        # spotskillmanager.pick("bottle")
+        # # Fetch the arm joint at grasping location
+        # ee_orientation_at_grasping = spotskillmanager.gaze_controller.env.ee_orientation_at_grasping
+        # spotskillmanager.nav("test_desk")
+        # spotskillmanager.place("test_desk", orientation_at_grasping) # This controls the arm initial orientation
         spotskillmanager.place(place_target, is_local=is_local, visualize=False)
         skill_log = spotskillmanager.place_controller.skill_result_log
         if "num_steps" not in skill_log:
