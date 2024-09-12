b52c566afd13d627ffa8f835404eb2f75872e5ec
Tushar Sangam <tusharsangam5@gmail.com>
2024-09-12 16:56:00 -0700

    enabling nav pick nav place on new CG

--- spot_rl_experiments/experiments/skill_test/test_view_pose_based_nav.py
+++ spot_rl_experiments/experiments/skill_test/test_view_pose_based_nav.py
@@ -20,8 +20,18 @@ from spot_wrapper.utils import get_angle_between_two_vectors
 
 NUM_REPEAT = 2
 # Center, extent, object_tags
-WAYPOINT_TEST = [([3.4, -0.7, 0.5], [0.5, 0.3, 0.2], ["chair"])] * NUM_REPEAT  # x, y
-receptacle_name = "kitchen_island"
+WAYPOINT_TEST = [
+    (
+        [2.1, 5.3, 0.1],
+        [1.0, 0.8, 0.7],
+        ["wooden cabinet"],
+        [3.4, -0.7, 0.5],
+        [0.5, 0.3, 0.2],
+        ["chair"],
+    )
+] * NUM_REPEAT  # x, y
+object_to_pickup = "bottle"
+receptacle_name = "cabinet_to_kitchen_island"
 VISUALIZE = True
 PATH_PLANNING_VISUALIZATION_FOLDER = "path_planning_vis_for_cg"
 os.makedirs(PATH_PLANNING_VISUALIZATION_FOLDER, exist_ok=True)
@@ -65,6 +75,55 @@ class SpotRosSkillExecutor:
             "angle_facing_target": angle_facing_target,
         }
 
+    def nav(self, bbox_center, bbox_extent, query_class_names, metrics_list):
+        bbox_center = np.array(bbox_center)
+        bbox_extent = np.array(bbox_extent)
+        # Get the view poses
+        view_poses = get_view_poses(bbox_center, bbox_extent, query_class_names, False)
+        # Get the robot x, y, yaw
+        x, y, _ = self.spotskillmanager.spot.get_xy_yaw()
+        # Get the navigation points
+        nav_pts = get_navigation_points(
+            view_poses,
+            bbox_center,
+            bbox_extent,
+            [x, y],
+            VISUALIZE,
+            osp.join(PATH_PLANNING_VISUALIZATION_FOLDER, f"{receptacle_name}.png"),
+        )
+        # breakpoint()
+        # Sequentially give the points
+        if len(nav_pts) > 0:
+            final_pt_i = len(nav_pts) - 1
+            agg_metrics = {
+                "num_steps": 0,
+                "distance": -1,
+                "dot_product_facing_target": -1,
+                "angle_facing_target": -1,
+            }
+            for pt_i, pt in enumerate(nav_pts):
+                x, y, yaw = pt
+                if pt_i == final_pt_i:
+                    # Do normal point nav with yaw for the final location
+                    succeded, msg = self.spotskillmanager.nav(x, y, yaw, False)
+                else:
+                    # Do dynamic point yaw here for the intermediate points
+                    succeded, msg = self.spotskillmanager.nav(x, y)
+                # Compute the metrics
+                traj = self.spotskillmanager.nav_controller.get_most_recent_result_log().get(
+                    "robot_trajectory"
+                )
+                metrics = self.compute_metrics(traj, np.array([x, y]))
+                agg_metrics["num_steps"] += metrics["num_steps"]
+                agg_metrics["distance"] = metrics["distance"]
+                agg_metrics["dot_product_facing_target"] = metrics[
+                    "dot_product_facing_target"
+                ]
+                agg_metrics["angle_facing_target"] = metrics["angle_facing_target"]
+                agg_metrics["suc"] = succeded
+            metrics_list.append(agg_metrics)
+        return metrics_list, succeded
+
     def benchmark(self):
         """ "Run the benchmark code to test skills"""
 
@@ -72,70 +131,40 @@ class SpotRosSkillExecutor:
         for waypoint in WAYPOINT_TEST:
             # Call the skill manager
             self.spotskillmanager = SpotSkillManager(
-                use_mobile_pick=True, use_semantic_place=False
+                use_mobile_pick=True, use_semantic_place=True
             )
 
-            bbox_center, bbox_extent, query_class_names = waypoint
-            bbox_center = np.array(bbox_center)
-            bbox_extent = np.array(bbox_extent)
-
-            # Get the view poses
-            view_poses = get_view_poses(
-                bbox_center, bbox_extent, query_class_names, False
-            )
-            # Get the robot x, y, yaw
-            x, y, _ = self.spotskillmanager.spot.get_xy_yaw()
-            # Get the navigation points
-            nav_pts = get_navigation_points(
-                view_poses,
+            (
                 bbox_center,
                 bbox_extent,
-                [x, y],
-                VISUALIZE,
-                osp.join(PATH_PLANNING_VISUALIZATION_FOLDER, f"{receptacle_name}.png"),
+                query_class_names,
+                bbox_center_place,
+                bbox_extent_place,
+                query_class_place,
+            ) = waypoint
+            metric_list, nav_suc = self.nav(
+                bbox_center, bbox_extent, query_class_names, metrics_list
             )
-            # breakpoint()
-            # Sequentially give the points
-            if len(nav_pts) > 0:
-                final_pt_i = len(nav_pts) - 1
-                agg_metrics = {
-                    "num_steps": 0,
-                    "distance": -1,
-                    "dot_product_facing_target": -1,
-                    "angle_facing_target": -1,
-                }
-                for pt_i, pt in enumerate(nav_pts):
-                    x, y, yaw = pt
-                    if pt_i == final_pt_i:
-                        # Do normal point nav with yaw for the final location
-                        succeded, msg = self.spotskillmanager.nav(x, y, yaw, False)
-                    else:
-                        # Do dynamic point yaw here for the intermediate points
-                        succeded, msg = self.spotskillmanager.nav(x, y)
-                    # Compute the metrics
-                    traj = self.spotskillmanager.nav_controller.get_most_recent_result_log().get(
-                        "robot_trajectory"
-                    )
-                    metrics = self.compute_metrics(traj, np.array([x, y]))
-                    agg_metrics["num_steps"] += metrics["num_steps"]
-                    agg_metrics["distance"] = metrics["distance"]
-                    agg_metrics["dot_product_facing_target"] = metrics[
-                        "dot_product_facing_target"
-                    ]
-                    agg_metrics["angle_facing_target"] = metrics["angle_facing_target"]
-                    agg_metrics["suc"] = succeded
-                metrics_list.append(agg_metrics)
+            if nav_suc:
+                self.spotskillmanager.pick(object_to_pickup)
+                metric_list, nav_suc = self.nav(
+                    bbox_center_place, bbox_extent_place, query_class_place, metric_list
+                )
+                if nav_suc:
+                    self.spotskillmanager.place(None)
 
             # breakpoint()
             # Reset
             self.spotskillmanager.dock()
 
-        # Compute the final number
-        for mm in agg_metrics:
-            data = [agg_metrics[mm] for agg_metrics in metrics_list]
-            _mean = round(np.mean(data), 2)
-            _std = round(np.std(data), 2)
-            print(f"{mm}: {_mean} +- {_std}")
+        if len(metric_list) > 0:
+            agg_metrics = metric_list[0]
+            # Compute the final number
+            for mm in agg_metrics:
+                data = [agg_metrics[mm] for agg_metrics in metrics_list]
+                _mean = round(np.mean(data), 2)
+                _std = round(np.std(data), 2)
+                print(f"{mm}: {_mean} +- {_std}")
 
 
 def main():
