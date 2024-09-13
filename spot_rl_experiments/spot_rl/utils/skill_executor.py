09fd91b72eb8a1c1cf5d49dd0de63a52561caa3a
Jimmy Yang <yangtsungyen@gmail.com>
2024-09-13 15:02:47 -0400

    change to new place skills

--- spot_rl_experiments/spot_rl/utils/skill_executor.py
+++ spot_rl_experiments/spot_rl/utils/skill_executor.py
@@ -118,8 +118,13 @@ class SpotRosSkillExecutor:
             # Use the following for the hardcode waypoint place
             # succeded, msg = self.spotskillmanager.place(0.6, 0.0, 0.4, is_local=True)
             # Call semantic place skills
+            skill_input = skill_input.split("_")[1]
+            print(f"input to place: {skill_input}")
             succeded, msg = self.spotskillmanager.place(
-                None, is_local=True, visualize=False, enable_waypoint_estimation=True
+                skill_input,
+                is_local=True,
+                visualize=False,
+                enable_waypoint_estimation=True,
             )
             self.reset_skill_name_input(skill_name, succeded, msg)
         elif skill_name == "opendrawer":
