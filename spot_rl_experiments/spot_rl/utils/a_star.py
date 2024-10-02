6f9603852f08c86ae924c5565954e35eb7ccd90a
Tushar Sangam <tusharsangam5@gmail.com>
2024-10-01 19:40:43 -0700

    modified a star for spacius traversal and range based terminating condition

--- spot_rl_experiments/spot_rl/utils/a_star.py
+++ spot_rl_experiments/spot_rl/utils/a_star.py
@@ -8,6 +8,23 @@ def heuristic(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
+def is_goal_within_local_range(current, goal, expand_range, height, width):
+    local_top = current[0] - expand_range
+    local_bottom = current[0] + expand_range
+    local_left = current[1] - expand_range
+    local_right = current[1] + expand_range
+    is_local_area_within_bounds = (
+        0 <= local_top < height
+        and 0 <= local_bottom < height
+        and 0 <= local_left < width
+        and 0 <= local_right < width
+    )
+    is_goal_in_local_area = (
+        local_top <= goal[0] <= local_bottom and local_left <= goal[1] <= local_right
+    )
+    return is_goal_in_local_area and is_local_area_within_bounds
+
+
 # A* algorithm implementation
 def astar(grid, start, goal):
     # Priority queue
@@ -26,11 +43,14 @@ def astar(grid, start, goal):
     # Directions for moving in the grid: up, down, left, right
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+    expand_range = (
+        2  # almost 0.6 m length & width, donnot change works well emperically
+    )
     while open_set:
         current = heapq.heappop(open_set)[1]
 
         # If the goal is reached, reconstruct the path
-        if current == goal:
+        if is_goal_within_local_range(current, goal, expand_range, *grid.shape[:2]):
             path = []
             while current in came_from:
                 path.append(current)
@@ -40,14 +60,41 @@ def astar(grid, start, goal):
 
         # Explore neighbors
         for direction in directions:
-            neighbor = (current[0] + direction[0], current[1] + direction[1])
+            neighbor_center = (current[0] + direction[0], current[1] + direction[1])
 
+            # each neightbour check expand_range X expand_range pixel around it for occupancy
             # Skip if the neighbor is out of bounds or occupied
-            if (
-                0 <= neighbor[0] < grid.shape[0]
-                and 0 <= neighbor[1] < grid.shape[1]
-                and grid[neighbor[0], neighbor[1]] == 0
-            ):
+            is_area_free = grid[neighbor_center[0], neighbor_center[1]] == 0
+            is_area_within_bounds = (
+                0 <= neighbor_center[0] < grid.shape[0]
+                and 0 <= neighbor_center[1] < grid.shape[1]
+            )
+
+            if is_area_free and is_area_within_bounds:
+                local_top = neighbor_center[0] - expand_range
+                local_bottom = neighbor_center[0] + expand_range
+                local_left = neighbor_center[1] - expand_range
+                local_right = neighbor_center[1] + expand_range
+                is_local_area_within_bounds = (
+                    0 <= local_top < grid.shape[0]
+                    and 0 <= local_bottom < grid.shape[0]
+                    and 0 <= local_left < grid.shape[1]
+                    and 0 <= local_right < grid.shape[1]
+                )
+                is_local_area_free = (
+                    grid[
+                        local_top : local_bottom + 1, local_left : local_right + 1
+                    ].sum()
+                    == 0
+                    if is_local_area_within_bounds
+                    else False
+                )
+                is_area_free = is_area_free and is_local_area_free
+                is_area_within_bounds = (
+                    is_area_within_bounds and is_local_area_within_bounds
+                )
+            neighbor = neighbor_center
+            if is_area_within_bounds and is_area_free:
 
                 # Tentative g_score for neighbor
                 tentative_g_score = g_score[current] + 1
