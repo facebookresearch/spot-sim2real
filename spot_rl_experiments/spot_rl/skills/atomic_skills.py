e3225cf191def1b491370e39df5907e81f367f72
Jimmy Yang <yangtsungyen@gmail.com>
2024-11-06 15:12:01 -0500

    polish

--- spot_rl_experiments/spot_rl/skills/atomic_skills.py
+++ spot_rl_experiments/spot_rl/skills/atomic_skills.py
@@ -191,10 +191,9 @@ class Skill:
             ]
             observations, _, done, info = self.env.step(action_dict=action_dict)  # type: ignore
 
-            if "None" not in human_action:
-                # Do not interrupt pick and place skills when human does something
-                if begin_skill_name not in ["pick", "place"]:
-                    done = True
+            # Do not interrupt pick and place skills when human does something
+            if "None" not in human_action and begin_skill_name not in ["pick", "place"]:
+                done = True
 
             curr_pose = [
                 self.env.x,  # type: ignore
