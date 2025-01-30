107035da42c6cb8b8bccb3c4ab01c5ff15c8e2c0
Jimmy Yang <jimmytyyang@meta.com>
2025-01-30 17:21:23 +0100

    only do interruption on pick and nav, and not place by human

--- spot_rl_experiments/spot_rl/skills/atomic_skills.py
+++ spot_rl_experiments/spot_rl/skills/atomic_skills.py
@@ -194,9 +194,10 @@ class Skill:
             observations, _, done, info = self.env.step(action_dict=action_dict)  # type: ignore
 
             # Do not interrupt pick and place skills when human does something
+            # update Jan 30: only do interrupt when robot is in pick or nav
             if (
                 "None" not in human_action
-                and begin_skill_name not in ["place", "pick"]
+                and begin_skill_name not in ["place"]
                 and not is_exploring
             ):
                 done = True
